7b979005a49634a70a4aca9cdce6991c2a631b58
tdy211 <1024545633@qq.com>
2020-10-04 12:32:23 +0800

    提交测试

--- mainwindow.h
+++ mainwindow.h
@@ -2,7 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include<QLabel>
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -17,5 +17,6 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QLabel labelTest;
 };
 #endif // MAINWINDOW_H
